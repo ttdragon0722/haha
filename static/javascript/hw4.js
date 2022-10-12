ec9dd425b2e5769c4e023a88ffa4facaeaa6bee9
ttdragon <a0909956502@gmail.com>
2022-10-12 15:21:54 +0800

    20221012作業4

---
+++ static/javascript/hw4.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    reload(1,()=>{
+        $("body").css("left", (window.innerWidth-800)/2);
+        console.log("sss")
+    }
+    )
+});
+
+function reload(time,func) {
+    for (let i = 1; i <= time; i++) {
+        window.setTimeout(function () {
+            if (true) {
+                func()
+                if (i == time) {
+                    reload(time,func)
+                }
+            }
+        }, 1000 * i);
+    }
+}
